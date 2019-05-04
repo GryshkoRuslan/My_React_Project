e5073ed3c63ed43379b6580b7ee1574671ebf325
GryshkoRuslan <sempay1313@gmail.com>
2019-05-04 11:33:39 +0300

    removed duplicate

--- src/components/tabsContents/TabContent1.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { StyledTabContent } from "styles/styledTabsParts";
-
-export const TabContent1 = () => (
-  <StyledTabContent>Item Two</StyledTabContent>
-);
--- src/components/tabsContents/TabContent2.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { StyledTabContent } from "styles/styledTabsParts";
-
-export const TabContent2 = () => (
-  <StyledTabContent>Item Three</StyledTabContent>
-);
